c2e58cca8d6656b220445aebf9f366d96efc1de6
Ella Karhi <100384206+ellakarhi@users.noreply.github.com>
2022-04-18 22:39:48 +0000

    Part 3 is getting work on

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 def is_leapyear(year):
-  if year % 4:
-   return True 
-  elif year % 100:
+  if year % 100 == 0:
     return True
-    if year % 400:
+    if year % 400 == 0:
       return True
     else:
       return False
-print (is_leapyear(2000))
+      
+  elif year % 4 == 0:
+    if year % 100 == 0:
+      if year % 400 != 0:
+       return False
+  elif year % 4 == 0:
+    if year % 100 != 0:
+      return False
+  else:
+    return False
+print (is_leapyear(4))
